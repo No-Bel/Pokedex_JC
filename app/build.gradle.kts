36a641348f23aac2ad1ced38389ac1f9f5d4573b
Nodar Belashvili <nodarbelashvili@gmail.com>
2024-05-04 18:18:50 +0400

    create project

---
+++ app/build.gradle.kts
@@ -0,0 +1,114 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id ("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.pokedex"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.pokedex"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.13"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Retrofit
+    //noinspection UseTomlInstead
+    implementation("com.squareup.retrofit2:retrofit:${rootProject.extra["retrofit_version"]}")
+    //noinspection UseTomlInstead
+    implementation("com.squareup.retrofit2:converter-gson:${rootProject.extra["retrofit_version"]}")
+    //noinspection UseTomlInstead
+    implementation("com.squareup.okhttp3:okhttp:${rootProject.extra["retrofit_okhttp3_version"]}")
+    //noinspection UseTomlInstead
+    implementation("com.squareup.okhttp3:logging-interceptor:${rootProject.extra["retrofit_okhttp3_version"]}")
+
+    // Timber
+    //noinspection UseTomlInstead
+    implementation("com.jakewharton.timber:timber:4.7.1")
+
+//    // Coroutines
+    //noinspection UseTomlInstead
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${rootProject.extra["coroutines_version"]}")
+    //noinspection UseTomlInstead
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${rootProject.extra["coroutines_version"]}")
+    //noinspection UseTomlInstead
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${rootProject.extra["lifecycle_version"]}")
+    //noinspection UseTomlInstead
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
+
+    // Coil
+    //noinspection UseTomlInstead
+    implementation("io.coil-kt:coil:1.1.1")
+    //noinspection UseTomlInstead
+    implementation("com.google.accompanist:accompanist-coil:0.7.0")
+
+    //Dagger - Hilt
+    //noinspection UseTomlInstead
+    implementation("com.google.dagger:hilt-android:${rootProject.extra["di_hilt"]}")
+    kapt("com.google.dagger:hilt-compiler:${rootProject.extra["di_hilt"]}")
+    //noinspection UseTomlInstead
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    //noinspection UseTomlInstead
+    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    kapt("com.google.dagger:hilt-android-compiler:${rootProject.extra["di_hilt"]}")
+
+    //noinspection UseTomlInstead
+//    implementation("com.android.support:palette-v7:28.0.0")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("room_version", "2.5.2")
+        set("di_hilt", "2.47")
+        set("retrofit_version", "2.11.0")
+        set("retrofit_okhttp3_version", "5.0.0-alpha.13")
+        set("coroutines_version", "1.7.1")
+        set("lifecycle_version", "2.7.0")
+    }
+}
+plugins {
+    id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id ("com.google.dagger.hilt.android") version "2.47" apply false
+}
