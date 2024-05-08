aab73c9387e3f0cf9bc6fda892f8762850323330
Nodar Belashvili <nodarbelashvili@gmail.com>
2024-05-08 13:40:09 +0400

    Searching Pokemon

--- app/src/main/java/com/example/pokedex/presentation/pokemonList/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokedex/presentation/pokemonList/PokemonListScreen.kt
@@ -56,7 +56,8 @@ import com.example.pokedex.presentation.util.Constants.POKEMON_DETAIL_SCREEN
 
 @Composable
 fun PokemonListScreen(
-    navController: NavController
+    navController: NavController,
+    viewModel: PokemonListViewModel = hiltViewModel()
 ) {
     Surface(
         color = MaterialTheme.colorScheme.background,
@@ -77,7 +78,7 @@ fun PokemonListScreen(
                     .fillMaxWidth()
                     .padding(16.dp)
             ) {
-
+                viewModel.searchPokemonList(it)
             }
             Spacer(modifier = Modifier.height(16.dp))
             PokemonList(navController = navController)
@@ -114,7 +115,7 @@ fun SearchBar(
                 .background(Color.White, CircleShape)
                 .padding(horizontal = 20.dp, vertical = 12.dp)
                 .onFocusChanged {
-                    isHintDisplayed = !it.isFocused
+                    isHintDisplayed = !it.isFocused && text.isNotEmpty()
                 }
         )
         if (isHintDisplayed) {
@@ -136,6 +137,7 @@ fun PokemonList(
     val endReached by remember { viewModel.endReached }
     val loadError by remember { viewModel.loadError }
     val isLoading by remember { viewModel.isLoading }
+    val isSearching by remember { viewModel.isSearching }
 
     LazyColumn(
         contentPadding = PaddingValues(16.dp)
@@ -146,7 +148,7 @@ fun PokemonList(
             pokemonList.size / 2 + 1
         }
         items(itemCount) {
-            if (it >= itemCount - 1 && !endReached) {
+            if (it >= itemCount - 1 && !endReached && !isLoading && !isSearching) {
                 viewModel.loadPokemonPaginated()
             }
             PokedexRow(rowIndex = it, entry = pokemonList, navController = navController)
